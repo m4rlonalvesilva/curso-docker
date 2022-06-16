67fdb5af307144f6afb1033a3ff4320da681900a
m4rlonalvesilva <marlonesx@gmail.com>
2022-06-16 15:12:44 -0300

    exercícios

--- 1_imagens_e_containers/copia/app.js
+++ 1_imagens_e_containers/copia/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Olá minha imagem!')
+  res.send('Olá minha imagem! Esse é um novo build')
 })
 
 app.listen(port, () => {
